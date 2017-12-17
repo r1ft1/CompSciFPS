a899d0973df3d259d31175b344b6fed1819d5df5
r1ft1 <jamessundaram8@gmail.com>
2017-12-17 20:53:37 +0000

    Repo contains all scripts

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Vector3 target;
+	public GameObject player;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		target = player.gameObject.GetComponent<Transform>().position;
+		transform.LookAt(target);
+	}
+}
